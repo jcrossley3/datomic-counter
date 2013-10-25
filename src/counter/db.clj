63092a14fcf12f3f27f317896c5401923520ff50
Jim Crossley <jim@crossleys.org>
2013-10-25 17:32:50 -0400

    Simplify the counter schema and add an increment tx fn

--- src/counter/db.clj
+++ src/counter/db.clj
@@ -3,15 +3,25 @@
             [datomic.api :as d]
             [clojure.java.io :as io]))
 
+(def uri (format "datomic:inf://localhost:%d/%s"
+                 (util/port :hotrod)
+                 (util/app-name)))
+
+(def conn (delay (d/connect uri)))
+
 (defn init
-  "Create the database, load the schema, and create a counter"
+  "Create the database, load the schema, and initialize counter"
   []
-  (let [uri (format "datomic:inf://localhost:%d/%s"
-                    (util/port :hotrod)
-                    (util/app-name))]
-    (if (d/create-database uri)
-      (let [c (d/connect uri)]
-        @(d/transact c (read-string (slurp (io/resource "schema.dtm"))))
-        @(d/transact c [{:db/id #db/id[:db.part/user]
-                         :counter/id :incrementer
-                         :counter/value 0}])))))
+  (when (d/create-database uri)
+    @(d/transact @conn (read-string (slurp (io/resource "schema.dtm"))))
+    @(d/transact @conn [{:db/id :counter :value 0}])))
+
+(defn value
+  "The current value of the counter"
+  []
+  (:value (d/entity (d/db @conn) :counter)))
+
+(defn increment
+  "Invoke a transaction function to increment the counter"
+  []
+  @(d/transact @conn [[:increment]]))
