69d4d168b66bd6708c6d8c41f57c8c89e2199846
Jim Crossley <jim@crossleys.org>
2013-10-25 18:19:35 -0400

    Dump and increment the counter every second on every node

--- src/counter/core.clj
+++ src/counter/core.clj
@@ -1,6 +1,20 @@
-(ns counter.core)
+(ns counter.core
+  (:require [counter.db :as db]
+            [immutant.jobs :as job]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn work
+  "Fires on schedule to dump/increment counter"
+  []
+  (println "counter value =" (db/value))
+  (db/increment))
+
+(defn start
+  "Initialize the database and schedule the counter"
+  []
+  (db/init)
+  (job/schedule "counter" work, :every :second, :singleton false))
+
+(defn stop
+  "Stop the counter"
+  []
+  (job/unschedule "counter"))
