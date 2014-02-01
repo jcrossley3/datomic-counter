8cb86b27c4a9f00b2be7bcf8081abb14ef6f5d91
Jim Crossley <jim@crossleys.org>
2014-02-01 12:19:54 -0500

    Make logging dep explicit

--- project.clj
+++ project.clj
@@ -4,6 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojure/tools.logging "0.2.6"]
                  [org.infinispan/infinispan-client-hotrod "6.0.0.Final"]
                  [com.datomic/datomic-pro "0.9.4314"]]
   :immutant {:init counter.core/start})
